2333f4fb9af34fb15b41d7e5b455f32029c30978
woftis <stevenwalker1991@googlemail.com>
2022-02-10 15:51:08 +0000

    first iteration done, full working game. no UI other than writes game result to a header field

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //function for computer opponent to randomly generate a move
 function computerPlay() {
     //get a number between 0 and 2 
@@ -27,28 +26,59 @@ function capitaliseFirstLetter(string){
 
 
 function playRound(playerSelection, computerSelection) {
-
+    
     //first check if it's a draw and handle that
     if (playerSelection === computerSelection) {
-        return('It\'s a draw!');  
+        return('Shit son, it\'s a draw!');  
     }
 
     //check each combination of player selections to check if they have won, otherwise return lose
     if (playerSelection === 'rock' && computerSelection === 'scissors'
         || playerSelection === 'paper' && computerSelection === 'rock'
         || playerSelection === 'scissors' && computerSelection === 'paper') {
-        
+            playerScore++;
             return (`You win! ${capitaliseFirstLetter(playerSelection)} beats ${capitaliseFirstLetter(computerSelection)}`);
         }
     else {
+        computerScore++;
         return (`You lose! ${capitaliseFirstLetter(computerSelection)} beats ${capitaliseFirstLetter(playerSelection)}`);
     }
-
 }
 
-//get the player selection via a prompt and lowercase to standardise input
-let playerSelection = prompt('What\'s your move, rock, paper or scissors?').toLowerCase();
-let computerSelection = computerPlay();
+function game() {
+    let playerSelection;
+    let computerSelection;
+
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt('What\'s your move, rock, paper or scissors?').toLowerCase();
+        computerSelection = computerPlay();
+
+        console.log(playRound(playerSelection, computerSelection));
+        //below will help to store results in html
+        //const results = document.getElementById('result');
+        //results.innerHTML = playRound(playerSelection, computerSelection);
+    }
+    console.log('player score: ' + playerScore + ' computer score: ' + computerScore);
+    gameResults(playerScore, computerScore);
+}
+
+function gameResults(playerScore, computerScore) {
+    console.log(playerScore);
+    console.log(computerScore);
+    let result = '';
+
+    if (playerScore === computerScore) {
+        result = `It was a draw! Why not try again? The score was Player:${playerScore} - Computer:${computerScore}`;
+    } else if (playerScore > computerScore) {
+        result  = `You won! Well done! The score was Player: ${playerScore} - Computer: ${computerScore}`;
+    } else {
+        result  = `Oooh sorry, you lost. The score was Player: ${playerScore} - Computer: ${computerScore}`;
+    }
+    console.log(result);
+    const results = document.getElementById('result-field');
+    results.innerHTML = result;
+
+}
 
 /* all the stuff below is for later on trying to drive stuff via the UI - need to figure out how to let DOM load first
 
@@ -62,8 +92,9 @@ playerInput.addEventListener('change', (event) => {
 })
 */
 
-const results = document.getElementById('result');
-results.innerHTML = playRound(playerSelection, computerSelection);
+let playerScore = 0;
+let computerScore = 0;
+game();
 
 
 
